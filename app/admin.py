95e01f9580913164446cccea85cd1be93db78445
waderwu <waderwu@163.com>
2016-10-15 21:22:28 +0800

    yihsuwang1.0
    仅仅是登陆的页面

---
+++ app/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import UserProfile
+
+
+class UserProfileAdmin(admin.ModelAdmin):
+    fields = ('user', 'description',)
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
